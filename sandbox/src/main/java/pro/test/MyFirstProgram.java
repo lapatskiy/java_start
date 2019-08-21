55245a6d756b9f9aab6adb3786f5c59930725963
lapatskiy <val@imastertek.com>
2019-08-21 15:57:19 -0700

    Added more code

---
+++ sandbox/src/main/java/pro/test/MyFirstProgram.java
@@ -0,0 +1,13 @@
+package pro.test;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        hello();
+    }
+    public static void hello() {
+        String somebody = "world";
+        System.out.println("Hello ," + somebody + "!");
+
+    }
+}
